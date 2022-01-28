8d9aceb376599d744df0c6a91d2ac2b7a566d87f
Numan Ibn Mazid <numanibnmazid@gmail.com>
2022-01-28 19:52:38 +0600

    Faker call simplified

--- backend/utils/management/commands/generate_dummy_users.py
+++ backend/utils/management/commands/generate_dummy_users.py
@@ -4,21 +4,14 @@ from django.db import transaction
 from django.core.management.base import BaseCommand
 
 
-info = {
-    "NUM_OF_DATA": 7,
-    "MODEL": get_user_model(),
-    "DELETE_OLD_DATA": False,
-}
-
-
 class Command(BaseCommand):
     help = "Generates dummy data"
 
     def _generate_dummy_data(self):
         # Create dummy data
         create_users_with_factory(
-            num_of_data=info.get("NUM_OF_DATA"),
-            delete_old_data=info.get("DELETE_OLD_DATA")
+            num_of_data=7,
+            delete_old_data=False
         )
 
     @transaction.atomic
